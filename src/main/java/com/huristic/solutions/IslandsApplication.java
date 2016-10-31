0fd5ea419994ff5fdaf49673031fe157fed41aab
David Khanaferov <dkhanaferov@opentable.com>
2016-10-30 21:36:35 -0700

    initial commit

---
+++ src/main/java/com/huristic/solutions/IslandsApplication.java
@@ -0,0 +1,12 @@
+package com.huristic.solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IslandsApplication.class, args);
+	}
+}
